0b87ead4f78eb4f2788ea30919369635a176374f
MacMini2 <djsucho@gmail.com>
2016-05-05 11:14:58 +0200

    Add setTemplateFile method in admin BaseControl

--- components/BaseControl.php
+++ components/BaseControl.php
@@ -6,5 +6,21 @@ use Nette\Application\UI;
 
 class BaseControl extends UI\Control
 {
+ 	/** @var string */
+	public $templateFile;
+	
+
+	/**
+	 * Set template file
+	 * 
+	 * @param string $template
+	 * @return \App\AdminModule\Components\AdminMenuControl\AdminMenuControl
+	 */
+	public function setTemplateFile($template)
+	{
+		$this->templateFile = $template;
+		
+		return $this;
+	}
 
 }
