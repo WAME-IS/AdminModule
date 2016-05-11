c47016592a4073b6244e76f710edd1c3a5f23ccb
MacMini2 <djsucho@gmail.com>
2016-05-11 10:10:44 +0200

    Edit BaseControl in adminModule

--- components/BaseControl.php
+++ components/BaseControl.php
@@ -3,18 +3,57 @@
 namespace App\AdminModule\Components;
 
 use Nette\Application\UI;
+use Wame\ComponentModule\Entities\ComponentInPositionEntity;
 
 class BaseControl extends UI\Control
 {
- 	/** @var string */
+  	/** @var string */
+	public $componentName;
+	
+	/** @var ComponentInPositionEntity */
+	public $componentInPosition;
+	
+	/** @var string */
 	public $templateFile;
 	
-
+	
+	/**
+	 * Set component name
+	 * 
+	 * @param string $componentName
+	 * @return \App\AdminModule\Components\BaseControl
+	 */
+	public function setComponentName($componentName)
+	{
+		$this->componentName = $componentName;
+		
+		return $this;
+	}
+	
+	
+	/**
+	 * Set component in position
+	 * 
+	 * @param ComponentInPositionEntity $componentInPosition
+	 * @return \App\AdminModule\Components\BaseControl
+	 */
+	public function setComponentInPosition($componentInPosition)
+	{
+		$this->componentInPosition = $componentInPosition;
+		
+		if ($componentInPosition->getParameter('template')) {
+			$this->setTemplateFile($componentInPosition->getParameter('template'));
+		}
+		
+		return $this;
+	}
+	
+	
 	/**
 	 * Set template file
 	 * 
 	 * @param string $template
-	 * @return \App\AdminModule\Components\AdminMenuControl\AdminMenuControl
+	 * @return \App\AdminModule\Components\BaseControl
 	 */
 	public function setTemplateFile($template)
 	{
