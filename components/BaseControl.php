2a86ee5e1b780c1c710891ab1bbb9c788aca7c93
MacMini2 <djsucho@gmail.com>
2016-05-12 13:46:28 +0200

    Edit Admin BaseControl extends Core BaseControl

--- components/BaseControl.php
+++ components/BaseControl.php
@@ -2,64 +2,7 @@
 
 namespace App\AdminModule\Components;
 
-use Nette\Application\UI;
-use Wame\ComponentModule\Entities\ComponentInPositionEntity;
-
-class BaseControl extends UI\Control
+class BaseControl extends \App\Core\Components\BaseControl
 {
-  	/** @var string */
-	public $componentName;
-	
-	/** @var ComponentInPositionEntity */
-	public $componentInPosition;
-	
-	/** @var string */
-	public $templateFile;
-	
-	
-	/**
-	 * Set component name
-	 * 
-	 * @param string $componentName
-	 * @return \App\AdminModule\Components\BaseControl
-	 */
-	public function setComponentName($componentName)
-	{
-		$this->componentName = $componentName;
-		
-		return $this;
-	}
-	
-	
-	/**
-	 * Set component in position
-	 * 
-	 * @param ComponentInPositionEntity $componentInPosition
-	 * @return \App\AdminModule\Components\BaseControl
-	 */
-	public function setComponentInPosition($componentInPosition)
-	{
-		$this->componentInPosition = $componentInPosition;
-		
-		if ($componentInPosition->getParameter('template')) {
-			$this->setTemplateFile($componentInPosition->getParameter('template'));
-		}
-		
-		return $this;
-	}
-	
-	
-	/**
-	 * Set template file
-	 * 
-	 * @param string $template
-	 * @return \App\AdminModule\Components\BaseControl
-	 */
-	public function setTemplateFile($template)
-	{
-		$this->templateFile = $template;
-		
-		return $this;
-	}
 
 }
