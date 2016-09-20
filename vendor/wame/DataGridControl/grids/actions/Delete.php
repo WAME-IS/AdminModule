06a57ba1edb01001ad023800c56f4354e7e2c921
Stanislav Červeňák <stano@wame.sk>
2016-09-20 12:32:29 +0200

    EDIT DataGrids

--- vendor/wame/DataGridControl/grids/actions/Delete.php
+++ vendor/wame/DataGridControl/grids/actions/Delete.php
@@ -3,17 +3,52 @@
 namespace Wame\AdminModule\Vendor\Wame\DataGridControl\Actions;
 
 use Wame\DataGridControl\BaseGridItem;
+use Wame\DataGridControl\DataGridControl;
 
 
 class Delete extends BaseGridItem
 {
+    /** @var string */
+    private $link;
+    
+    
+    /**
+     * Return link
+     * 
+     * @param DataGridControl $grid
+     * @return string
+     */
+    private function getLink($grid)
+    {
+        if ($this->link) {
+            return $this->link;
+        } else {
+            return ":{$grid->presenter->getName()}:delete";
+        }
+    }
+    
+    
+    /**
+     * Set link
+     * 
+     * @param type $link
+     * @return \Wame\AdminModule\Vendor\Wame\DataGridControl\Actions\Delete
+     */
+    public function setLink($link)
+    {
+        $this->link = $link;
+        
+        return $this;
+    }
+    
+    
     /** {@inheritDoc} */
 	public function render($grid)
     {
-		$grid->addAction('delete', '', ":{$grid->presenter->getName()}:delete")
-			->setIcon('delete')
-			->addAttributes(['data-position' => 'left', 'data-tooltip' => _('Delete')])
-			->setClass('btn btn-xs btn-icon btn-hover-danger tooltipped ajax-modal');
+		$grid->addAction('delete', '', $this->getLink($grid))
+                ->setIcon('delete')
+                ->addAttributes(['data-position' => 'left', 'data-tooltip' => _('Delete')])
+                ->setClass('btn btn-xs btn-icon btn-hover-danger tooltipped ajax-modal');
 		
 		return $grid;
 	}
--- vendor/wame/DataGridControl/grids/actions/Show.php
+++ vendor/wame/DataGridControl/grids/actions/Show.php
@@ -3,16 +3,52 @@
 namespace Wame\AdminModule\Vendor\Wame\DataGridControl\Actions;
 
 use Wame\DataGridControl\BaseGridItem;
+use Wame\DataGridControl\DataGridControl;
+
 
 class Show extends BaseGridItem
 {
+    /** @var string */
+    private $link;
+    
+    
+    /**
+     * Return link
+     * 
+     * @param DataGridControl $grid
+     * @return string
+     */
+    private function getLink($grid)
+    {
+        if ($this->link) {
+            return $this->link;
+        } else {
+            return ":{$grid->presenter->getName()}:show";
+        }
+    }
+    
+    
+    /**
+     * Set link
+     * 
+     * @param type $link
+     * @return \Wame\AdminModule\Vendor\Wame\DataGridControl\Actions\Show
+     */
+    public function setLink($link)
+    {
+        $this->link = $link;
+        
+        return $this;
+    }
+
+
     /** {@inheritDoc} */
 	public function render($grid)
 	{
-		$grid->addAction('show', '', ":{$grid->presenter->getName()}:show")
+		$grid->addAction('show', '', $this->getLink($grid))
 			->setIcon('visibility')
 			->addAttributes(['data-position' => 'left', 'data-tooltip' => _('Show')])
-			->setClass('btn btn-xs btn-icon btn-hover-default tooltipped');
+			->setClass('btn btn-xs btn-icon btn-hover-info tooltipped');
 		
 		return $grid;
 	}
