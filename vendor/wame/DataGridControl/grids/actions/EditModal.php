a5965a504666f21bde6e3a6eac9804be60de27f4
Stanislav Červeňák <stano@wame.sk>
2016-10-20 16:21:22 +0200

    EDIT datagrid edit modal action

--- vendor/wame/DataGridControl/grids/actions/EditModal.php
+++ vendor/wame/DataGridControl/grids/actions/EditModal.php
@@ -10,18 +10,18 @@ class EditModal extends BaseGridAction
 		$grid->addAction('edit', '', $this->getLink($grid))
 			->setIcon('edit')
 			->addAttributes($this->getAttributes() + [
-                'data-position' => 'left', 
+                'data-position' => 'left',
                 'data-tooltip' => _('Edit')
             ])
-			->setClass('btn btn-xs btn-icon btn-hover-warning tooltipped ajax-modal');
-		
+			->setClass('btn btn-xs btn-icon btn-hover-warning tooltipped ajax-modal ajax-modal-fixed');
+
 		return $grid;
 	}
-    
+
     /** {@inheritDoc} */
     protected function getLinkAction()
     {
         return 'edit';
     }
-    
+
 }
