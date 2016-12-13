fb0d8cbe2e354c77eb5be980e8d5c688d3048c71
shadd0w <rene.gmitter@wame.sk>
2016-12-13 15:00:18 +0100

    POLISH DashboardPresenter

--- presenters/DashboardPresenter.php
+++ presenters/DashboardPresenter.php
@@ -4,16 +4,6 @@ namespace App\AdminModule\Presenters;
 
 class DashboardPresenter extends \App\AdminModule\Presenters\BasePresenter
 {
-    public function startup()
-    {
-        parent::startup();
-		
-		if (!$this->user->isAllowed('dashboard', 'view')) {
-			$this->flashMessage(_('To enter this section you have sufficient privileges.'), 'danger');
-			$this->redirect('parent');
-		}
-    }
-	
 	public function renderDefault()
 	{
 		$this->template->siteTitle = _('Dashboard');
